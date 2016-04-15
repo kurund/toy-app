94b3b7f89db82a27981b99162800370ce12c0f45
Kurund Jalmi <kurund@yahoo.com>
2016-04-15 18:09:17 +0530

    users can be added / listed / edited / deleted, also class roster are listed, plus it good to commit before I mess up my working code :)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,51 @@
-import {Component} from 'angular2/core';
+import {Component}       from 'angular2/core';
+import {HeroService}     from './hero.service';
+import {RosterService}     from './roster.service';
+import {StudentService}     from './student.service';
+import {HeroesComponent} from './heroes.component';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {DashboardComponent} from './dashboard.component';
+import {HeroDetailComponent } from './hero-detail.component';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a [routerLink]="['Dashboard']">Class Roaster</a>
+      <a [routerLink]="['Heroes']">Users</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app/app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService,
+    RosterService,
+    StudentService
+  ]
 })
-export class AppComponent { }
+
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
+])
+
+export class AppComponent {
+  title = 'Toy App';
+}
