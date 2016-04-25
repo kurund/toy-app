975caf5b1ec86f57a46c6b85374ab360328fc2b3
Kurund Jalmi <kurund@yahoo.com>
2016-04-26 01:12:49 +0530

    student model fixes

--- app/mock-students.ts
+++ app/mock-students.ts
@@ -1,8 +1,9 @@
 import {Student} from './student';
 
 export var STUDENTS: Student[] = [
-  { "id": 1, "hero_id": 1, "roster_id": 1 },
-  { "id": 2, "hero_id": 2, "roster_id": 1 },
-  { "id": 3, "hero_id": 3, "roster_id": 2 },
-  { "id": 4, "hero_id": 4, "roster_id": 2 }
+  { "id": 1, "hero_id": 1, "roster_id": 1, "first_name": '', "last_name": '' },
+  { "id": 2, "hero_id": 2, "roster_id": 1, "first_name": '', "last_name": '' },
+  { "id": 3, "hero_id": 3, "roster_id": 2, "first_name": '', "last_name": '' },
+  { "id": 4, "hero_id": 4, "roster_id": 2, "first_name": '', "last_name": '' }
 ];
+
